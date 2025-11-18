ea48d43de864281d368963f4649a280cdd034d71
Joshua <ejembijoshman@gmail.com>
2025-11-18 19:22:51 +0100

    feat:update notification table structure

---
+++ Migrations/20251118180548_AddPushNotificationTokensUpdate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WSFBackendApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPushNotificationTokensUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "EnableOutlineNotifications",
+                table: "PushNotificationTokens",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EnableOutlineNotifications",
+                table: "PushNotificationTokens");
+        }
+    }
+}
