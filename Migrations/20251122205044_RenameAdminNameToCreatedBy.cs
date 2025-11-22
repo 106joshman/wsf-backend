884c23ca8017ce782f0a3e4a76f62fa9362fec5c
Joshua <ejembijoshman@gmail.com>
2025-11-22 21:52:13 +0100

    fix:updet with new migration

---
+++ Migrations/20251122205044_RenameAdminNameToCreatedBy.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WSFBackendApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameAdminNameToCreatedBy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AdminName",
+                table: "Teachings");
+
+            migrationBuilder.RenameColumn(
+                name: "AdminId",
+                table: "Teachings",
+                newName: "CreatedById");
+
+            migrationBuilder.RenameColumn(
+                name: "AdminName",
+                table: "PrayerOutlines",
+                newName: "CreatedBy");
+
+            migrationBuilder.RenameColumn(
+                name: "AdminId",
+                table: "PrayerOutlines",
+                newName: "CreatedById");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Teachings",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Teachings");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedById",
+                table: "Teachings",
+                newName: "AdminId");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedById",
+                table: "PrayerOutlines",
+                newName: "AdminId");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedBy",
+                table: "PrayerOutlines",
+                newName: "AdminName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AdminName",
+                table: "Teachings",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
